3c1de05a979abcc3de78c0530f16fff094b5dcfb
manikk <MANIKK@MANIKK-PC.(none)>
2012-01-11 03:47:23 +0530

    Initial Commit

---
+++ src/com/phonegap/peyar/App.java
@@ -0,0 +1,14 @@
+package com.phonegap.peyar;
+import com.phonegap.DroidGap;
+import android.os.Bundle;
+import com.phonegap.*;
+
+public class App extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+        
+    }
+}
